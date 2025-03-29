a4feaf3acaf0aec835b1121300c8c76fec843940
Thiago Andrade <tja.usa.202@gmail.com>
2025-03-29 13:24:06 -0400

    fix something

--- src/routes/map.routes.ts
+++ src/routes/map.routes.ts
@@ -12,9 +12,5 @@ router.post("/stations", getStations);
 router.post("/stations/nearest", getNearestStations);
 router.post("/stations/details", getDetails);
 router.post("/stations/search", searchHandler);
-<<<<<<< HEAD
-router.post("/stations/route", findPath);
 //router.post("/trips", findPath);
-=======
->>>>>>> a3216038d68739752edc398008b54510fb287371
 export default router;
