f09a44393493bac4242d433638298caf5c8d674d
Thiago <tja.usa.202@gmail.com>
2025-03-30 18:09:36 -0400

    stop tracking prisma (devs should pull prisma schema themselves)

--- prisma/migrations/20250309172440_added/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `refreshTokenCreatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "refreshTokenCreatedAt" TIMESTAMP(3) NOT NULL;
--- prisma/migrations/20250309172613_fixed/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "refreshTokenCreatedAt" DROP NOT NULL;
