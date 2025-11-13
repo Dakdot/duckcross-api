80c93289e7c99af3a161cc0ce724329143e0f6db
Thiago Andrade <tja.usa.202@gmail.com>
2025-11-13 11:59:35 -0500

    Begin migration to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+import { defineConfig } from "eslint/config";
+import _import from "eslint-plugin-import";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends("eslint:recommended"),
+
+    plugins: {
+      import: fixupPluginRules(_import),
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
+
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+
+    rules: {
+      "comma-dangle": 0,
+      "no-underscore-dangle": 0,
+      "no-param-reassign": 0,
+      "no-return-assign": 0,
+      camelcase: 0,
+      "import/extensions": 0,
+      "@typescript-eslint/no-redeclare": 0,
+      "@typescript-eslint/quotes": "off",
+      "@typescript-eslint/comma-dangle": "warn",
+    },
+  },
+]);
